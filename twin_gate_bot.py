3ed27580c9aef7c9af308cb4f88392739be30aa6
cis <don.m.wen@gmail.com>
2025-06-10 00:17:48 +0800

    Add twin_gate_bot.py - Original working version

---
+++ twin_gate_bot.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import os
+import requests
+import time
+from datetime import datetime
+
+BOT_TOKEN = "7151382731:AAGb1r6ACJE-xkMxFiW4Ml3wD1X5rKOPTkc"
+
+print('🚀 Twin Gate Bot 啟動中...')
